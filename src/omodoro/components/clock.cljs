d22c55b34aa4080b994b370a66a7553798331836
Fredrik Antell <fred@fredantell.com>
2014-05-31 11:41:06 -0700

    Refactor clock into its own component

--- src/omodoro/components/clock.cljs
+++ src/omodoro/components/clock.cljs
@@ -1,20 +1,42 @@
 (ns omodoro.components.clock
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [chan <! timeout]]))
 
-#_(defn commit-widget [app owner opts]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil "I'm a clock widget"))))
+(defn tick! [data]
+  (let [tick-once! (fn []
+                     (when-not (zero? (:seconds @data))
+                       (om/transact! data :seconds dec)))]
+    (js/setInterval tick-once! 1000)))
 
-(defn commit-widget [app owner opts]
+(defn pad [n]
+  (if (<= 0 n 9)
+    (str "0" n)
+    (str n)))
+
+(defn clock [seconds]
+  (let [minutes (int (/ seconds 60))
+        seconds (rem seconds 60)]
+    (dom/p #js {:id "clock"}
+           (str (pad minutes) ":" (pad seconds)))))
+
+(defn reset-button [app owner interval]
+  (dom/button #js {:onClick
+                   (fn [e]
+                     (om/update! app :seconds (* 25 60)))}
+              "reset"))
+
+(defn clock-widget [app owner opts]
   (reify
-    om/IRender
-    (render [_]
+    om/IInitState
+    (init-state [_]
+      {:interval nil})
+    om/IDidMount
+    (did-mount [_]
+      (om/set-state! :interval (tick! app)))
+    om/IRenderState
+    (render-state [_ {:keys [interval]}]
       (dom/div nil
-        (dom/h1 nil "Today, I am commiting to")
-        (dom/button nil "-")
-        (dom/h2 nil "0")
-        (dom/button nil "+")
-        (dom/div nil "Pomodoros")))))
+        (reset-button app owner interval)
+        (clock (:seconds app))))))
+
--- src/omodoro/core.cljs
+++ src/omodoro/core.cljs
@@ -1,54 +1,32 @@
 (ns omodoro.core
   (:require [cljs.nodejs :as node]
+            [cljs.core.async :refer [chan <! timeout]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.nodejs :as n]
-            [omodoro.components.dailycommit :as dc]))
+            [omodoro.components.dailycommit :as dc]
+            [omodoro.components.clock :as clock]))
 
 (enable-console-print!)
+
+;; Next steps:
+;; Factor out clock entirely to a component
+;; Start arranging components in a guided workflow
 
 (def gui (n/require "nw.gui"))  ;; kernel_time.core.gui.Window.get().zoomLevel = 2
 (def state (atom {:seconds (* 25 60)
                   :commitment 4}))
 
-(defn tick! [data]
-  (let [tick-once! (fn []
-                     (when-not (zero? (:seconds @data))
-                       (om/transact! data :seconds dec)))]
-    (js/setInterval tick-once! 1000)))
-
-(defn pad [n]
-  (if (<= 0 n 9)
-    (str "0" n)
-    (str n)))
-
-(defn clock [seconds]
-  (let [minutes (int (/ seconds 60))
-        seconds (rem seconds 60)]
-    (dom/p #js {:id "clock"}
-           (str (pad minutes) ":" (pad seconds)))))
-
+(defn render-app [app owner opts]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+               (om/build clock/clock-widget app)
+               (om/build dc/commit-widget app)))))
 
 (om/root
- (fn [app-state owner]
-   (reify
-     om/IInitState
-     (init-state [this]
-       {:interval nil})
-     om/IDidMount
-     (did-mount [this]
-       (om/set-state! owner :interval (tick! app-state)))
-     om/IRenderState
-     (render-state [this {:keys [interval]}]
-       (dom/div nil
-         (dom/button #js {:onClick
-                          (fn [e]
-                            (js/clearInterval interval)
-                            (om/update! app-state :seconds (* 25 60))
-                            (om/set-state! owner :interval (tick! app-state)))}
-                     "reset")
-         (clock (:seconds app-state))
-         (om/build dc/commit-widget app-state)))))
+ render-app
  state
  {:target (. js/document (getElementById "root"))})
 
