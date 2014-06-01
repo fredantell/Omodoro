335188b8992491e18812151aa7e3d518a06bf36a
Fredrik Antell <fred@fredantell.com>
2014-05-31 18:50:46 -0700

    Add settings to app state

--- src/omodoro/core.cljs
+++ src/omodoro/core.cljs
@@ -18,20 +18,29 @@
 ;; Add component to display day's goal and day's progress
 ;; Start arranging components in a guided workflow
 
+;; Gamification - give prizes for who has longest streak
+;; prop bets vs other players - bet you can't do 5 days in a row
+;; 
+
 (def gui (n/require "nw.gui"))  ;; kernel_time.core.gui.Window.get().zoomLevel = 2
-(def state (atom {:seconds (* 25 60)
-                  :commitment 4
-                  :completed 1
-                  :current-task nil}))
+(def state (atom {:app {:seconds (* 25 60)
+                        :commitment 4
+                        :completed 1
+                        :current-task nil}
+                  :settings {:timer-only? :true
+                             :pom-length 25
+                             :short-break 5
+                             :long-break 30
+                             :ticking? :true}}))
 
 (defn render-app [app owner opts]
   (reify
     om/IRender
     (render [_]
       (dom/div nil
-               (om/build pmtr/pomodoro-meter app)
-               (om/build clock/clock-widget app)
-               (om/build dc/commit-widget app)))))
+               (om/build pmtr/pomodoro-meter (:app app))
+               (om/build clock/clock-widget (:app app))
+               (om/build dc/commit-widget (:app app))))))
 
 (om/root
  render-app
