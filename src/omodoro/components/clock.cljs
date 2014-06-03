93f314ebf9be652548d5e4ef3e4bec79714410a4
Fredrik Antell <fred@fredantell.com>
2014-06-02 20:55:53 -0700

    Start working on pause hover

--- src/omodoro/components/clock.cljs
+++ src/omodoro/components/clock.cljs
@@ -4,6 +4,12 @@
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [chan <! >! put! timeout]]))
 
+;; Next Steps:
+;; Add hover play/pause interaction
+;; Add ability to pause and reset
+;; Add ability to one off override duration of a cycle
+;; If the pomodoro is paused for more than 3 minutes then auto reset
+
 (defn tick! [data]
   (let [tick-once! (fn []
                      (when-not (zero? (:seconds @data))
@@ -18,8 +24,12 @@
 (defn clock [seconds]
   (let [minutes (int (/ seconds 60))
         seconds (rem seconds 60)]
-    (dom/p #js {:id "clock"}
-           (str (pad minutes) ":" (pad seconds)))))
+    (dom/div #js {:id "clock-container"}
+      (dom/div #js {:id "clock"}
+        (dom/div #js {:className "overlay"}
+                 (dom/div nil (dom/i #js {:className "fa fa-pause"})))
+        (str (pad minutes) ":" (pad seconds))
+        ))))
 
 (defn reset-button [app owner interval]
   (dom/button #js {:onClick
